526137fae7b401b0fc66be84c99122a01178ab02
Linus Karsai <linusk@thoughtworks.com>
2019-03-12 16:26:12 +1100

    Create technologies from json

---
+++ src/topoInterface.ts
@@ -0,0 +1,11 @@
+export const createBox = () => {
+  // stub for creating box
+};
+
+export const createSystem = () => {
+  // stub for creating box
+};
+
+export const createTechnology = (id: string, name: string) => {
+  // stub for creating box
+};
