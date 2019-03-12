e340a12fae76b2a752e92394a1b8e6a650154f80
Linus Karsai <linusk@thoughtworks.com>
2019-03-12 14:22:41 +1100

    Add tslint, prettier and copyright headers

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -1,8 +1,24 @@
-import cli, {checkFile, parseJsonFileContent} from "./cli";
-import { mockProcessStdout } from "jest-mock-process";
+/*
+ * Copyright 2019 Thoughtworks Inc. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import cli, { checkFile, parseJsonFileContent } from './cli';
+import { mockProcessStdout } from 'jest-mock-process';
 const CLIOUTPUT_INDEX = 2;
 
-describe("cli", () => {
+describe('cli', () => {
   let mockStdout: any;
   afterEach(() => {
     mockStdout.mockRestore();
@@ -13,57 +29,61 @@ describe("cli", () => {
   });
 
   // commented out because of issues updating snapshots
-  xit("startup text should match snapshot", () => {
+  xit('startup text should match snapshot', () => {
     cli();
     expect((mockStdout as any).mock.calls).toMatchSnapshot();
   });
 
-  it("check that options are shown when no arguments are given", () => {
+  it('check that options are shown when no arguments are given', () => {
     cli();
     expect(JSON.stringify((mockStdout as any).mock.calls)).toContain('Usage:');
-    expect(JSON.stringify((mockStdout as any).mock.calls)).toContain('Options:');
-  })
+    expect(JSON.stringify((mockStdout as any).mock.calls)).toContain(
+      'Options:'
+    );
+  });
 
-  it("get upset if no file is provided", () => {
+  it('get upset if no file is provided', () => {
     const args = ['--host', 'http://fakeurl', '--port', '2000'];
     cli(args);
-    expect(JSON.stringify((mockStdout as any).mock.calls)).toContain('Error: Please provide a file');
+    expect(JSON.stringify((mockStdout as any).mock.calls)).toContain(
+      'Error: Please provide a file'
+    );
   });
 
-  describe("checkFile functionality", () => {
-    it("if valid json file provided, no error thrown", () => {
-      const filename = "hey/its/me/te.st.json";
+  describe('checkFile functionality', () => {
+    it('if valid json file provided, no error thrown', () => {
+      const filename = 'hey/its/me/te.st.json';
       expect(() => checkFile(filename)).not.toThrow();
-    })
+    });
 
-    it("if file is not .json format, throw an exception", () => {
-      const filename = "test.text";
+    it('if file is not .json format, throw an exception', () => {
+      const filename = 'test.text';
       expect(() => checkFile(filename)).toThrow();
-    })
+    });
 
-    it("if .json is in the path but it is not a valid json file, throw an exception", () => {
-      const filename = ".json/test.text";
+    it('if .json is in the path but it is not a valid json file, throw an exception', () => {
+      const filename = '.json/test.text';
       expect(() => checkFile(filename)).toThrow();
-    })
+    });
 
-    it("if file does not have a filename, throw an exception", () => {
-      const filename = "apples";
+    it('if file does not have a filename, throw an exception', () => {
+      const filename = 'apples';
       expect(() => checkFile(filename)).toThrow();
-    })
+    });
   });
 
-  describe("parseJsonFileContent functionality", () => {
-    it("if file content is invalid JSON, throw exception", () => {
+  describe('parseJsonFileContent functionality', () => {
+    it('if file content is invalid JSON, throw exception', () => {
       const badJson = `"data":{"type": "string"}`;
       expect(() => parseJsonFileContent(badJson)).toThrow();
-    })
+    });
 
-    it("if file content is valid JSON, return parsed json object", () => {
+    it('if file content is valid JSON, return parsed json object', () => {
       const goodJson = `{"data":{"type": "string"}}`;
       const returnedJsonObj = parseJsonFileContent(goodJson);
 
-      const expectedJsonObj = { data: {"type": "string"}};
+      const expectedJsonObj = { data: { type: 'string' } };
       expect(returnedJsonObj).toEqual(expectedJsonObj);
-    })
-  })
+    });
+  });
 });
--- src/cli.ts
+++ src/cli.ts
@@ -1,39 +1,57 @@
-import chalk from "chalk";
-import figlet from "figlet";
-import commander from "commander";
-const fs = require('fs');
+/*
+ * Copyright 2019 Thoughtworks Inc. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+import chalk from 'chalk';
+import commander from 'commander';
+import figlet from 'figlet';
+import fs from 'fs';
 
 const cli = (args: any = process.argv) => {
   console.log(
     chalk.yellow(
-      figlet.textSync("TOPO JSON Collector", { horizontalLayout: "full" })
+      figlet.textSync('TOPO JSON Collector', { horizontalLayout: 'full' })
     )
   );
-   commander
-    .description("Please select one of the following actions: ")
-    .option("-f, --file <file>", "Parse JSON file")
-    .option("--host <host>", "Set host")
-    .option("--port <port>", "Set port")
+  commander
+    .description('Please select one of the following actions: ')
+    .option('-f, --file <file>', 'Parse JSON file')
+    .option('--host <host>', 'Set host')
+    .option('--port <port>', 'Set port')
     .parse(process.argv);
 
-    let jsonObject;
+  let jsonObject;
 
-    try {
-      if (!commander.file) throw new Error("Please provide a file");
+  try {
+    if (!commander.file) {
+      throw new Error('Please provide a file');
+    }
 
-      checkFile(commander.file);
-      fs.readFile(commander.file, 'utf8', (err: any, data: any) => {
-        try {
-          if (err) throw new Error('File not found: ' + commander.file);
-          jsonObject = parseJsonFileContent(data);
-          console.log(chalk.green("SUCCESS: Your file is valid."))
-          console.log(JSON.stringify(jsonObject));
+    checkFile(commander.file);
+    fs.readFile(commander.file, 'utf8', (err: any, data: any) => {
+      try {
+        if (err) {
+          throw new Error('File not found: ' + commander.file);
         }
-        catch (error) {
-          errorMessage(error);
-        }
-      });
+        jsonObject = parseJsonFileContent(data);
+        console.log(chalk.green('SUCCESS: Your file is valid.'));
+        console.log(JSON.stringify(jsonObject));
+      } catch (error) {
+        errorMessage(error);
+      }
+    });
   } catch (error) {
     errorMessage(error);
   }
@@ -42,20 +60,21 @@ const cli = (args: any = process.argv) => {
 const errorMessage = (message: string) => {
   console.log(chalk.red(message));
   commander.outputHelp();
-}
+};
 
 const checkFile = (file: string) => {
-  const fileExtension = file.substring((file.lastIndexOf('.')), file.length);
-  if (fileExtension !== '.json') throw new Error('Please provide a valid .json file.')
-}
+  const fileExtension = file.substring(file.lastIndexOf('.'), file.length);
+  if (fileExtension !== '.json') {
+    throw new Error('Please provide a valid .json file.');
+  }
+};
 
 const parseJsonFileContent = (fileContent: string) => {
   try {
     return JSON.parse(fileContent);
-  }
-  catch (error) {
+  } catch (error) {
     throw new Error('JSON is not valid for file: ' + commander.file);
   }
-}
+};
 export default cli;
-export {checkFile, parseJsonFileContent};
+export { checkFile, parseJsonFileContent };
