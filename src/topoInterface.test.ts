bca15fd1b131adaea963a3e665ccd4e54c0c44b7
Linus Karsai <linusk@thoughtworks.com>
2019-03-13 16:05:27 +1100

    Create topoInterface with graphql mutations
    architecture-topography/topo#11

--- src/topoInterface.test.ts
+++ src/topoInterface.test.ts
@@ -1,7 +1,79 @@
-// import nock from 'nock';
+import {request} from 'graphql-request';
+import TopoInterface from './topoInterface';
+jest.mock('graphql-request');
+
+const HOST = 'http://FAKEHOST/';
 
 describe('topoInterface', () => {
-  it('empty test', () => {
-    expect(true).toBe(false);
+  let topoInterface: TopoInterface;
+
+  beforeEach(() => {
+    topoInterface = new TopoInterface(HOST);
+    (request as any).mockClear();
+  });
+
+  it('Call correct mutation for createBox', async () => {
+    const MUTATION = `
+      mutation create($name: String! $id: String! $boxType: BoxType! $parentId: String){
+        createBox(name: $name, id: $id, boxType: $boxType, parentId: $parentId) {
+          name
+          id
+        }
+      }
+    `;
+
+    await topoInterface.createBox('test-001', 'Test Box', 'Platform');
+
+    expect(request).toBeCalledWith(HOST, MUTATION, {
+      boxType: 'Platform',
+      id: 'test-001',
+      name: 'Test Box',
+    });
+  });
+
+  it('Call correct mutation for createTechnology', async () => {
+    const MUTATION = `
+      mutation create($name: String! $id: String!){
+        createTechnology(
+          name: $name
+          id: $id
+        ) {
+          name
+          id
+        }
+      }
+    `;
+
+    await topoInterface.createTechnology('tech-react', 'React');
+
+    expect(request).toBeCalledWith(HOST, MUTATION, {
+      id: 'tech-react',
+      name: 'React',
+    });
+  });
+
+  it('Call correct mutation for createSystem', async () => {
+    const MUTATION = `
+      mutation create($name: String! $id: String! $parentBoxId: String! $technologies: [String!]){
+        createSystem(
+          name: $name
+          id: $id
+          parentBoxId: $parentBoxId
+          technologies: $technologies
+        ) {
+          name
+          id
+        }
+      }
+    `;
+
+    await topoInterface.createSystem('sys-001', 'Cool System', ['react'], 'parent-id');
+
+    expect(request).toBeCalledWith(HOST, MUTATION, {
+      id: 'sys-001',
+      name: 'Cool System',
+      technologies: ['react'],
+      parentBoxId: 'parent-id',
+    });
   });
 });
