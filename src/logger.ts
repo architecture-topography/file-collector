3d36bec0a9a552f05c81d281d40dbc12d2f6396f
Linus Karsai <linusk@thoughtworks.com>
2019-03-15 09:58:28 +1100

    Add ability to set loglevel via LOGLEVEL env variable

--- src/logger.ts
+++ src/logger.ts
@@ -2,6 +2,27 @@ import Logger from 'js-logger';
 
 Logger.useDefaults();
 
+const loglevel = process.env.LOGLEVEL;
+if (loglevel) {
+  switch(loglevel) {
+    case 'DEBUG':
+      Logger.setLevel(Logger.DEBUG);
+      break;
+    case 'ERROR':
+      Logger.setLevel(Logger.ERROR);
+      break;
+    case 'INFO':
+      Logger.setLevel(Logger.INFO);
+      break;
+    case 'WARN':
+      Logger.setLevel(Logger.WARN);
+      break;
+    case 'TRACE':
+      Logger.setLevel(Logger.TRACE);
+      break;
+  }
+}
+
 export const createLogger = (name: string) => {
   return Logger.get(name);
 };
